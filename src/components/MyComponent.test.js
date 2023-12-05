d7bd1591ce23c1f3069b4f38dcd3e71f63bf3402
mia-westcliff <143572221+mia-westcliff@users.noreply.github.com>
2023-12-04 20:30:56 -0800

    in class work

--- src/components/MyComponent.test.js
+++ src/components/MyComponent.test.js
@@ -6,4 +6,33 @@ describe("MyComponent", () => {
   it("should render correctly", () => {
     const component = shallow(<MyComponent />);
   });
+  it("should render initial layout", () => {
+    const component = shallow(<MyComponent />);
+    expect(component.getElement()).toMatchSnapshot();
+  });
+  it("should create an entry in component state", () => {
+    // given
+    const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    // when
+    form.props().onChange({target: {
+      name: 'myName',
+      value: 'myValue'
+    }});
+    // then
+    expect(component.state('input')).toBeDefined();
+  });
+  it("should create an entry in component state with the correct value", () => {
+    // given
+    const component = shallow(<MyComponent />);
+    const form = component.find('input');
+    // when
+    form.props().onChange({target: {
+      name: 'myName',
+      value: 'myValue'
+    }});
+    // then
+    expect(component.state('input')).toEqual('myValue');
+  });
+  
 });
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,4 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+let { configure } = require('enzyme');
+let Adapter = require('enzyme-adapter-react-16');
 
 configure({ adapter: new Adapter() });
